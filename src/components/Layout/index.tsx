f977b16b20a86d1a1aa8fde1dbecc1dc855d079c
Henrique Balmant <henrique.balmant@gmail.com>
2022-03-23 00:25:45 -0300

    feat: add setIsLoading useEffect

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -14,7 +14,7 @@ const Layout: React.FC = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      // setIsLoading(false);
+      setIsLoading(false);
     }, 1000);
   }, []);
 
--- src/components/LeftColumn/ProfilePanel/index.tsx
+++ src/components/LeftColumn/ProfilePanel/index.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import Panel from '../../Panel';
+import Panel from "../../Panel";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const ProfilePanel: React.FC = () => {
   return (
     <Panel>
       <Container>
-        <div className="profile-cover"></div>
+        <div className='profile-cover'></div>
         <img
-          src="https://github.com/guilhermerodz.png"
-          alt="Avatar"
-          className="profile-picture"
+          src='https://avatars.githubusercontent.com/u/20211646?v=4'
+          alt='Avatar'
+          className='profile-picture'
         />
-        <h1>Guilherme Rodz</h1>
-        <h2>Software Engineer @ Rocketseat</h2>
+        <h1>Henrique Balmant</h1>
+        <h2>Backend Engineer @ Conta Simples</h2>
 
-        <div className="separator"></div>
+        <div className='separator'></div>
 
-        <div className="key-value">
-          <span className="key">Quem viu seu perfil</span>
-          <span className="value">1.558</span>
+        <div className='key-value'>
+          <span className='key'>Quem viu seu perfil</span>
+          <span className='value'>1.558</span>
         </div>
-        <div className="key-value">
-          <span className="key">Viram sua publicação</span>
-          <span className="value">388</span>
+        <div className='key-value'>
+          <span className='key'>Viram sua publicação</span>
+          <span className='value'>388</span>
         </div>
       </Container>
     </Panel>
